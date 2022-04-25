f901c8e0c42a0a1edc32d92276d54f6053787e30
Jimmy <66128107+jimmyorz@users.noreply.github.com>
2022-04-26 03:02:11 +0800

    Delete Open Instagram App.js

--- Scripts/Open Instagram App.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Open Instagram App
-// @version      1.0.0
-// @author       jimmyorz
-// @match        *://*.instagram.com/*
-// ==/UserScript==
-window.location.href = `instagram://${window.location.pathname.slice(1)}${
-	window.location.search
-}${window.location.hash}`;
